b110dcbb9af3fa115bfd1253b23159d8edd4032b
YuMin29 <f61434@gmail.com>
2024-07-17 11:20:02 +0800

    test switch region

---
+++ buyandship.py
@@ -0,0 +1,62 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class BuyAndShip:
+
+    def __init__(self, url):
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("detach", True)
+        driver = webdriver.Chrome(options=options)
+        driver.maximize_window()
+        driver.get(url)
+        self.driver = driver
+
+    def click_region_element(self):
+        regionElement = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(
+                (By.CLASS_NAME, "bs-header-top-bar__content__country")
+            )
+        )
+
+        if regionElement is None:
+            print("not find region")
+        else:
+            regionElement.click()
+
+    def switch_region(self, target_region):
+        print("Start to switch region => ", target_region)
+
+        region_list = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_all_elements_located(
+                (By.CLASS_NAME, "bs-language-list__item")
+            )
+        )
+
+        if region_list is None:
+            print("not find region_list")
+        else:
+            print("region list leh => ", len(region_list))
+
+        for region_item in region_list:
+            region_name = region_item.find_element(
+                By.CLASS_NAME, "bs-language-list__item__name"
+            )
+            print("item name => ", region_name.text.strip())
+
+            if region_item.is_displayed() and region_item.is_enabled():
+                if region_item.text in target_region:
+                    region_item.click()
+                    new_region = WebDriverWait(self.driver, 5).until(
+                        EC.presence_of_element_located(
+                            (By.CLASS_NAME, "bs-header-top-bar__content__country__text")
+                        )
+                    )
+                    print("new region => ", new_region.text)
+                    return new_region
+
+    def quit_chrome(self):
+        self.driver.quit()
---
+++ test_buyandship.py
@@ -0,0 +1,34 @@
+from buyandship import BuyAndShip
+import pytest
+
+tw_url = "https://www.buyandship.com.tw/"
+
+
+@pytest.mark.parametrize(
+    "test_region, target_region",
+    [
+        ("香港 - 正體中文", "香港 - 繁體中文"),
+        ("香港 - English", "Hong Kong - English"),
+        ("澳門 - 正體中文", "澳門 - 繁體中文"),
+        ("台灣 - 正體中文", "台灣 - 正體中文"),
+        ("日本 - 日本語", "日本 - 日本語"),
+        ("新加坡 - English", "Singapore - English"),
+        ("馬來西亞 - English", "Malaysia - English"),
+        ("印度 - English", "India - English"),
+        ("阿聯酋 - English", "UAE - English"),
+        ("澳洲 - English", "Australia - English"),
+        ("菲律賓 - English", "Philippines - English"),
+        ("英國 - English", "United Kingdom - English"),
+        ("越南 - Tiếng Việt", "Vietnam - tiếng Việt"),
+    ],
+)
+def test_switch_region_from_tw(test_region, target_region):
+    buyAndShip = BuyAndShip(tw_url)
+    try:
+        buyAndShip.click_region_element()
+        new_region = buyAndShip.switch_region(test_region)
+        assert new_region.text in target_region
+        buyAndShip.quit_chrome()
+    except Exception as e:
+        buyAndShip.quit_chrome()
+        pytest.fail(f"Test failed due to exception: {repr(e)}")
